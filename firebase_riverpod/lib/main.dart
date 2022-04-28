aba7b5c452c025611e738f6267f7f122c233e353
r0227n <developryo@gmail.com>
2022-04-29 07:53:10 +0900

    add: add Provider to notify attendess

--- firebase_riverpod/lib/main.dart
+++ firebase_riverpod/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      const ProviderScope(
+          child: MyApp(),
+      ),
+  );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
