3186ee34549f25fbb31863f8b8f5e136799cae5b
r0227n <developryo@gmail.com>
2022-04-29 04:33:25 +0900

    add: add initialize project file

--- firebase_riverpod/lib/main.dart
+++ firebase_riverpod/lib/main.dart
@@ -3,11 +3,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(
-      const ProviderScope(
-          child: MyApp(),
-      ),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
