37caeed5cc9e25f17d81ab5b0ed6902f6b4a7138
r0227n <developryo@gmail.com>
2022-12-26 07:56:11 +0900

    edit: update ios settings

--- receive_sharing_intent_example/ios/example/ShareViewController.swift
+++ receive_sharing_intent_example/ios/example/ShareViewController.swift
@@ -9,11 +9,11 @@ class ShareViewController: SLComposeServiceViewController {
     let sharedKey = "ShareKey"
     var sharedMedia: [SharedMediaFile] = []
     var sharedText: [String] = []
-    let imageContentType = UTType.image.preferredFilenameExtension ?? ""
-    let videoContentType = kUTTypeMovie as String
-    let textContentType = kUTTypeText as String
-    let urlContentType = kUTTypeURL as String
-    let fileURLType = kUTTypeFileURL as String;
+    let imageContentType = UTType.image.identifier
+    let videoContentType = UTType.movie.identifier
+    let textContentType = UTType.text.identifier
+    let urlContentType = UTType.url.identifier
+    let fileURLType = UTType.fileURL.identifier
 
     override func isContentValid() -> Bool {
         return true
@@ -30,7 +30,7 @@ class ShareViewController: SLComposeServiceViewController {
         if let content = extensionContext!.inputItems[0] as? NSExtensionItem {
             if let contents = content.attachments {
                 for (index, attachment) in (contents).enumerated() {
-                    if attachment.hasItemConformingToTypeIdentifier(imageContentType) {
+                    if attachment.hasItemConformingToTypeIdentifier(imageContentType)  {
                         handleImages(content: content, attachment: attachment, index: index)
                     } else if attachment.hasItemConformingToTypeIdentifier(textContentType) {
                         handleText(content: content, attachment: attachment, index: index)
@@ -345,95 +345,3 @@ extension Array {
         return Int(index) < count ? self[Int(index)] : nil
     }
 }
-
-//import UIKit
-//import Social
-//import MobileCoreServices
-//import Foundation
-//
-//class ShareViewController: SLComposeServiceViewController {
-//
-//    let hostAppBundleIdentifier = "com.example.shareExtension"
-//    let sharedKey = "ShareKey"
-//    var sharedText: [String] = []
-//    let urlContentType = kUTTypeURL as String
-//
-//    override func isContentValid() -> Bool {
-//        // Do validation of contentText and/or NSExtensionContext attachments here
-//        return true
-//    }
-//
-//    override func didSelectPost() {
-//        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-//        if let content = extensionContext!.inputItems[0] as? NSExtensionItem {
-//            if let contents = content.attachments {
-//                for (index, attachment) in (contents).enumerated() {
-//                    if attachment.hasItemConformingToTypeIdentifier(urlContentType) {
-//                        handleUrl(content: content, attachment: attachment, index: index)
-//                    }
-//                }
-//            }
-//        } else {
-//            // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-//            self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-//        }
-//    }
-//
-//    override func configurationItems() -> [Any]! {
-//        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-//        return []
-//    }
-//
-//    private func handleUrl (content: NSExtensionItem, attachment: NSItemProvider, index: Int) {
-//        attachment.loadItem(forTypeIdentifier: urlContentType, options: nil) { [weak self] data, error in
-//
-//            if error == nil, let item = data as? URL, let this = self {
-//
-//                this.sharedText.append(item.absoluteString)
-//
-//                // If this is the last item, save imagesData in userDefaults and redirect to host app
-//                if index == (content.attachments?.count)! - 1 {
-//                    let userDefaults = UserDefaults(suiteName: "group.\(this.hostAppBundleIdentifier)")
-//                    userDefaults?.set(this.sharedText, forKey: this.sharedKey)
-//                    userDefaults?.synchronize()
-//                    this.redirectToHostApp(type: .text)
-//                }
-//            } else {
-//                self?.dismissWithError()
-//            }
-//        }
-//    }
-//
-//    private func dismissWithError() {
-//        print("[ERROR] Error loading data!")
-//        let alert = UIAlertController(title: "Error", message: "Error loading data", preferredStyle: .alert)
-//
-//        let action = UIAlertAction(title: "Error", style: .cancel) { _ in
-//            self.dismiss(animated: true, completion: nil)
-//        }
-//
-//        alert.addAction(action)
-//        present(alert, animated: true, completion: nil)
-//        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-//    }
-//
-//    enum RedirectType {
-//        case media
-//        case text
-//        case file
-//    }
-//
-//    private func redirectToHostApp(type: RedirectType) {
-//        let url = URL(string: "ShareMedia://dataUrl=\(sharedKey)#\(type)")
-//        var responder = self as UIResponder?
-//        let selectorOpenURL = sel_registerName("openURL:")
-//
-//        while (responder != nil) {
-//            if (responder?.responds(to: selectorOpenURL))! {
-//                let _ = responder?.perform(selectorOpenURL, with: url)
-//            }
-//            responder = responder!.next
-//        }
-//        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-//    }
-//}
